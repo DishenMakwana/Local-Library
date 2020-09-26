2b65d284736f0121fd430aa13d63306d5527b6a1
DishenMakwana <58837408+DishenMakwana@users.noreply.github.com>
2020-09-26 08:24:26 +0530

    Update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u$0blzt-cup(w16f31x!+^j$mg$m8z
 #DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []  ##
+ALLOWED_HOSTS = ['https://sheltered-ocean-30487.herokuapp.com/','127.0.0.1']  ##
 
 
 # Application definition
