965167f887c90a10304c81c287ff49451156bea2
DishenMakwana <dishenmakwana.dm@gmail.com>
2020-11-12 22:20:12 +0530

    version 1.1.0

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -17,21 +17,19 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'u$0blzt-cup(w16f31x!+^j$mg$m8zvm3*&(dq^%6u)*mt&ien')
-#SECRET_KEY = 'u$0blzt-cup(w16f31x!+^j$mg$m8zvm3*&(dq^%6u)*mt&ien'
+# SECRET_KEY = 'u$0blzt-cup(w16f31x!+^j$mg$m8zvm3*&(dq^%6u)*mt&ien'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+# DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
-
 
 # Application definition
 
@@ -76,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -86,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -119,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -150,3 +144,9 @@ EMAIL_HOST_USER = 'demo.django.dd@gmail.com'
 DEFAULT_FROM_EMAIL = 'demo.django.dd@gmail.com'
 SERVER_EMAIL = 'demo.django.dd@gmail.com'
 EMAIL_HOST_PASSWORD = 'demo@django'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
